b8b2bd4f9c94667340af2c05d799f65307af23d9
Carlos.Nunes <carloslnunes@gmail.com>
2012-12-23 15:47:44 +0000

    Added missing images folder. Moved more deadbeef core functions calls to app delegate.

--- macosx/DBAppDelegate.h
+++ macosx/DBAppDelegate.h
@@ -1,21 +1,10 @@
-/*
-    DeaDBeeF Cocoa GUI
-    Copyright (C) 2012 Carlos Nunes <carloslnunes@gmail.com>
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
+//
+//  DBAppDelegate.h
+//  deadbeef
+//
+//  Created by Carlos Nunes on 4/28/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
 
 #import <Cocoa/Cocoa.h>
 
@@ -36,11 +25,12 @@
 - (IBAction) previousAction: (id)sender;
 - (IBAction) togglePlay: sender;
 
+
 @property (assign) IBOutlet NSWindow *mainWindow;
 @property (assign) IBOutlet DBTableView *mainPlaylist;
 
 
-// helper functions
+// deadbeef core wrapper functions
 
 + (NSArray *) supportedFormatsExtensions;
 
@@ -51,5 +41,15 @@
 + (BOOL) insertDirectory : (NSArray*) list;
 
 + (void) movePlayListItems : (NSIndexSet*) rowIndexes row:(NSInteger) rowBefore;
++ (void) setCursor : (NSInteger) cursor;
++ (void) clearPlayList;
+
++ (void) setVolumeDB : (float) value;
++ (float) volumeDB;
++ (float) minVolumeDB;
+
++ (int) intConfiguration : (NSString *) key num:(NSInteger) def;
+
++ (void) setIntConfiguration : (NSString *) key value:(NSInteger) def;
 
 @end
