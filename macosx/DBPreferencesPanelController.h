6085e1a1d1dff01db957dbcc570628529c83efbe
Carlos.Nunes <carloslnunes@gmail.com>
2013-09-17 00:50:44 +0100

    Added empty preferences panel to project

---
+++ macosx/DBPreferencesPanelController.h
@@ -0,0 +1,16 @@
+//
+//  DBPreferencesWindowController.h
+//  deadbeef
+//
+//  Created by Carlos Nunes on 8/30/13.
+//  Copyright 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface DBPreferencesPanelController : NSWindowController <NSWindowDelegate> {
+
+}
+
+@end
