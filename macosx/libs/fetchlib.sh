80e557251cc045a5989217f5cc612e1b0e18281a
Utilizador convidado <Guest@I02CP2014.fe.up.pt>
2015-01-02 17:31:11 +0000

    Started work on bash script to fetch needed external libraries tarballs

---
+++ macosx/libs/fetchlib.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+url[0] = 'http://www.nih.at/libzip/libzip-0.11.2.tar.gz'
+url[1] = 'http://www.wavpack.com/wavpack-4.70.0.tar.bz2'
+url[2] = 'ftp://ftp.mars.org/pub/mpeg//libmad-0.15.1b.tar.gz'
+url[3] = 'http://www.mega-nerd.com/SRC/libsamplerate-0.1.8.tar.gz'
+url[4] = 'http://www.mega-nerd.com/libsndfile/files/libsndfile-1.0.25.tar.gz'
+url[5] = 'http://downloads.xiph.org/releases/flac/flac-1.3.1.tar.xz'
+url[6] = 'http://downloads.sourceforge.net/project/libcddb/libcddb/1.3.2/libcddb-1.3.2.tar.bz2'
+
+
+#curl -o foo.pdf 'http://server1.cyberciti.biz/foo.pdf'
